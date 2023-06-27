0822d769d35b815db48246a3bdd6d1f75c3b8ce1
B4NG8ANG <02inho02@gmail.com>
2023-06-27 20:50:14 +0900

    타이틀 임시 구현
    1. 임시 이미지로 타이틀 화면을 만들고 씬 전환을 구현했습니다.

---
+++ Assets/Scripts/Title/titleManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class titleManager : MonoBehaviour
+{
+    public AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.Play();   
+    }
+}
