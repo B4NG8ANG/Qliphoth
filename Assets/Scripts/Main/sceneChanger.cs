cbdd6cbf558b6d6cc4b6a7fb8a08ab7d3741a61b
B4NG8ANG <02inho02@gmail.com>
2023-08-20 08:37:31 +0900

    설정씬 생성 및 설정 버튼 클릭시 설정씬 진입 기능 구현
    1. 설정씬을 생성하였습니다.
    2. 메인씬 상단에 설정 버튼을 생성하였고, 해당 버튼을 클릭시 설정씬으로 진입하도록 구현하였습니다.

--- Assets/Scripts/Main/sceneChanger.cs
+++ Assets/Scripts/Main/sceneChanger.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class enterPlay : MonoBehaviour
+public class sceneChanger : MonoBehaviour
 {
 
     public void onPlayButtonClick()
     {
         //SceneManager.LoadScene("Play");
         SceneChangeEffectManager.instance.FadeToScene("Play");
+    }
+
+    public void onSettingButtonClick()
+    {
+        SceneChangeEffectManager.instance.FadeToScene("Setting");
     }
 }
